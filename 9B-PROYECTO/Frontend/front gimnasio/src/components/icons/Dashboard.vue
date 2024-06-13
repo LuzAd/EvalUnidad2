f8afed2ddba1689833961925d4ef0128e57c86eb
LuzAd <lreyesgonzalez94@gmail.com>
2024-06-13 11:41:32 -0600

    Editar componentes

--- 9B-PROYECTO/Frontend/front gimnasio/src/components/icons/Dashboard.vue
+++ 9B-PROYECTO/Frontend/front gimnasio/src/components/icons/Dashboard.vue
@@ -34,8 +34,7 @@
             href="#"
             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
           >
-            <span class="material-symbols-outlined">
-home
+            <span class="material-symbols-outlined">home
 </span>
             <RouterLink to="/dashboard">
               <span class="ms-3">Dashboard</span>
@@ -512,7 +511,7 @@ handyman
     <div
       class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"
     >
-      <h1>Bienvenido al sistema</h1>
+      <h1>Bienvenido al sistemA</h1>
       
     </div>
     
